488cafa470b12e0ea2ffde52adc53de9d606184c
TripleNine666 <72222961+TripleNine666@users.noreply.github.com>
2023-01-07 14:30:00 +0300

    coolsite lesson 15 Class view (ListView, DetailView, CreateView)

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -2,11 +2,11 @@ from django.urls import path, re_path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='home'), # http://127.0.0.1:8000/
+    path('', WomenHome.as_view(), name='home'), # http://127.0.0.1:8000/
     path('about/', about, name='about'), # http://127.0.0.1:8000/about
-    path('addpage/', addpage, name='add_page'), # http://127.0.0.1:8000/add_page
+    path('addpage/', AddPage.as_view(), name='add_page'), # http://127.0.0.1:8000/add_page
     path('contact/', contact, name='contact'), # http://127.0.0.1:8000/contact
     path('login/', login, name='login'), # http://127.0.0.1:8000/login
-    path('page/<slug:post_slug>/', show_post, name='post'), # http://127.0.0.1:8000/post
-    path('category/<slug:cat_slug>/', show_category, name='category'), # http://127.0.0.1:8000/post
+    path('page/<slug:post_slug>/', ShowPost.as_view(), name='post'), # http://127.0.0.1:8000/post
+    path('category/<slug:cat_slug>/', WomenCategory.as_view(), name='category'), # http://127.0.0.1:8000/post
 ]
