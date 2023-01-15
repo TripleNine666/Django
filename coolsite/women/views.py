c154ecb78bcaec9fa372915b91626d8bd4624414
TripleNine666 <72222961+TripleNine666@users.noreply.github.com>
2023-01-15 12:01:47 +0300

    coolsite lesson 20 Login+Logout update

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import logout, login
+from django.contrib.auth.views import LoginView
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
@@ -51,10 +52,6 @@ def contact(request):
     return HttpResponse("Связаться с разработчиком")
 
 
-def login(request):
-    return HttpResponse("Авторизация")
-
-
 class ShowPost(DataMixin, DetailView):
     model = Women
     template_name = 'women/post.html'
@@ -100,3 +97,25 @@ class RegisterUser(DataMixin, CreateView):
         c_def = self.get_user_context(title='Регистрация')
         return context | c_def
 
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
+
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = 'women/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Авторизация")
+        return context | c_def
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
